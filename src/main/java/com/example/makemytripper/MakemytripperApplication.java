96e089e14f1cc1bf40a3207b049c3d3f468eee4b
pranavdhumal9987 <pranavdhumal9987@gmail.com>
2026-05-16 17:49:51 +0530

    test

---
+++ src/main/java/com/example/makemytripper/MakemytripperApplication.java
@@ -0,0 +1,13 @@
+package com.example.makemytripper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakemytripperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakemytripperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/makemytripper/MakemytripperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makemytripper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemytripperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
